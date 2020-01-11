c1afb5c4bca0679f2636cc69e6a84c148cb228e1
SpaceStrix <40722064+SpaceStrix@users.noreply.github.com>
2020-01-12 02:43:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,217 @@
+// Числа. Задачи.
+// 1. Получить число pi из Math и округлить его до 2-х знаков после точки
+
+let num = Math.PI.toFixed(2);
+console.log(num);
+
+// 2. Используя Math, найти максимальное и минимальное числа из представленного ряда 15, 11, 16, 12, 51, 12, 13, 51
+
+console.log(Math.max(22, 54, 84, 5, 15, 6, 3));
+console.log(Math.min(22, 54, 84, 5, 15, 6, 3));
+
+// 3. Работа с Math.random:
+
+// a. Получить случайное число и округлить его до двух цифр после запятой
+console.log(Math.random().toFixed(2));
+// b. Получить случайное целое число от 0 до X. X - любое произвольное число.
+console.log(Math.floor(Math.random() * 10));
+
+// 4. Проверить результат вычисления 0.6 + 0.7 - как привести к нормальному виду (1.3)?
+console.log((0.6 + 0.7).toFixed(2));
+// 5. Получить число из строки ‘100$’
+
+console.log(typeof parseInt("100$"));
+
+/* =============================// Числа. Задачи.============================= */
+
+// Строки. Методы.
+
+// 1. Получить первую и последнюю буквы строки  / 2. Сделать первую и последнюю буквы в верхнем регистре
+
+let str = "some test string";
+console.log(str[0].toUpperCase());
+console.log(str[str.length - 1].toUpperCase());
+
+// 3. Найти положение слова ‘string’ в строке
+console.log(str.indexOf("string"));
+
+// 4. Найти положение второго пробела (“вручную” ничего не считать)
+console.log(str.indexOf(" ", str.indexOf(" ") + 1));
+
+// 5. Получить строку с 5-го символа длиной 4 буквы
+console.log(str.substr(5, 4));
+
+// 6. Получить строку с 5-го по 9-й символы
+console.log(str.substring(5, 9));
+
+// 7. Получить новую строку из исходной путем удаления последних 6-и символов (то есть исходная строка без последних 6и символов)
+console.log(str.slice(0, -6));
+// 8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет содержаться текст “2016”
+let a = 20;
+let b = 16;
+console.log(typeof (a + b).toString(a + b));
+/* =============================// Строки. Методы.============================= */
+
+// Основы. Объекты.
+
+// 1,2,3 . Создать объект с полем product, равным ‘iphone’
+
+let obj = {
+  product: "iphone",
+  price: 100,
+  currensy: "dollar",
+  details: {
+    model: 6,
+    color: "red"
+  }
+};
+
+console.log(obj);
+
+// Основы. Выражения. Конструкция if else
+// 1. Если переменная равна “hidden”, присвоить ей значение “visible”, иначе - “hidden”.
+let value = "hidden";
+if (value == "hidden") {
+  value = "visible";
+} else {
+  value = "hidden";
+}
+
+console.log(value);
+
+// 2. Используя if, записать условие:
+let numValue = 5;
+//  если переменная равна нулю, присвоить ей 1;
+if (numValue == 0) {
+  console.log(1);
+  //   если меньше нуля - строку “less then zero”;
+} else if (numValue < 0) {
+  console.log("less then zero");
+  //   если больше нуля - используя оператор “присвоение”, переменную умножить на 10
+} else if (numValue > 0) {
+  console.log(numValue * 10);
+}
+
+/* 3. Дан объект let car = { name: 'Lexus', age: 10, create: 2008, needRepair: false }. 
+Написать условие если возраст машины больше 5 лет то нужно вывести в консоль сообщение 'Need Repair' и свойство needRepair в объекте car изменить на true; иначе изменить на false.
+ */
+
+let car = {
+  name: "Lexus",
+  age: 6,
+  create: 2008,
+  needRepair: false
+};
+
+if (car.age > 5) {
+  console.log("Need Repair");
+  car.needRepair = true;
+} else {
+  console.log(car.needRepair);
+}
+
+/* 4. Дан объект let item = { name: 'Intel core i7', price: '100$', discount: '15%' }.
+Написать условие если у item есть поле discount и там есть значение то в объекте item создать поле priceWithDiscount и записать туда цену с учетом скидки и вывести ее в консоль, обратите внимание  что поля discount и price это строки и вам из них нужно получить числа чтобы выполнить расчет. иначе если поля discount нет то вывести просто поле price в консоль. */
+
+let item = {
+  name: "Intel core i7",
+  price: "100$",
+  discount: "15%"
+};
+
+if (item.discount) {
+  item.priceWithDiscount =
+    (parseInt(item.price) / 100) * (100 - parseInt(item.discount));
+  console.log(item.priceWithDiscount + "$");
+} else {
+  console.log(item.price);
+}
+
+/* 5. Дан следующий код:
+
+let product = {
+    name: “Яблоко”,
+    price: “10$”
+};
+
+let min = 10; // минимальная цена
+let max = 20; // максимальная цена
+
+Написать условие если цена товара больше или равна минимальной цене и меньше или равна максимальной цене то вывести в консоль название этого товара, иначе вывести в консоль что товаров не найдено. */
+
+let product = {
+  name: "Яблоко",
+  price: parseInt("15")
+};
+let min = 10;
+let max = 20;
+
+/* if (product.price >= min && product.price <= max) {
+  console.log(`Товар со стоимостью ${product.price}$ это ${product.name}`);
+} else {
+  console.log(`Товар со стоимостью ${product.price}$ не найден`);
+} */
+
+product.price >= min && product.price <= max
+  ? console.log(`Товар со стоимостью ${product.price}$ это ${product.name}`)
+  : console.log(`Товар со стоимостью ${product.price}$ не найден`);
+
+/* =============================// Основы. Объекты.============================= */
+
+// Основы. Тернарный оператор. Switch Case
+
+/* 1. 
+Записать в виде switch case следующее условие:
+if (a === ‘block’) {
+	console.log(‘block’)
+} else if (a === ‘none’) {
+	console.log(‘none’)
+} else if (a === ‘inline’) {
+console.log(‘inline’)
+} else {
+	console.log(‘other’)
+}
+Записать условие, используя конструктор switch. В консоли должно отразиться только одно значение.
+ */
+
+let display = "block";
+switch (display) {
+  case "block":
+    console.log("block");
+    break;
+  case "none":
+    console.log("none");
+    break;
+  case "inline":
+    console.log("inline");
+    break;
+  case "inline-block":
+    console.log("inline-block");
+    break;
+
+  default:
+    console.log("without any changes");
+    break;
+}
+
+// Из задач по условному оператору if else выполнить задачи 1 и 2 в виде тернарного оператора.
+let condition = "hidden" ? "visible" : "hidden";
+console.log(condition);
+
+let tern = 0;
+tern = tern === 0 ? 1 : tern < 0 ? "less then zero" : tern * 10;
+console.log(tern);
+
+/* =============================// Основы. Тернарный оператор. Switch Case.============================= */
+
+// Основы. Циклы.
+// 1. На основе строки “i am in the easycode” сделать новую строку где первые буквы каждого слова будут в верхнем регистре. Использовать for или while.
+
+let strC = "i am in the easycode";
+let newStrC = "";
+for (let i = 0; i < strC.length - 1; i++) {
+  newStrC += strC[i].toUpperCase();
+}
+console.log(newStrC);
+
+/* =============================// Основы. Циклы.============================= */
